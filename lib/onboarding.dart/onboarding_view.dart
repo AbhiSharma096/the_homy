5dcc230fce6332122199609a58a34e906012f5b6
AbhiSharma096 <Abhisheksharma10a@gmail.com>
2024-04-21 22:21:04 +0530

    some changes

--- lib/onboarding.dart/onboarding_view.dart
+++ lib/onboarding.dart/onboarding_view.dart
@@ -1,13 +1,10 @@
-
-
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:the_homy/auth/login_or_signup.dart';
 import 'package:the_homy/component/color.dart';
 import 'package:the_homy/onboarding.dart/onboarding_items.dart';
-import 'package:the_homy/pages/signup_page.dart';
-
 
 class OnboardingView extends StatefulWidget {
   const OnboardingView({super.key});
@@ -19,67 +16,98 @@ class OnboardingView extends StatefulWidget {
 class _OnboardingViewState extends State<OnboardingView> {
   final controller = OnboardingItems();
   final pageController = PageController();
+  List number = [];
+  Map<dynamic, dynamic> Benefits={};
 
   bool isLastPage = false;
 
   @override
   Widget build(BuildContext context) {
+    DatabaseReference _ref = FirebaseDatabase.instance
+        .ref()
+        .child('Data')
+        .child('Combos')
+        .child('1');
+    // _ref.onValue.listen((event) {
+    //   setState(() {
+    //     number = event.snapshot.child('Benefit');
+    //   });
+    // });
+
+    _ref.onValue.listen((event) {
+      setState(() {
+        final data = event.snapshot.value as Map<dynamic, dynamic>?;
+        if (data != null) {
+          final benefitsMap = data.map(
+            (key, value) => MapEntry(key.toString(), value),
+          );
+          Benefits = benefitsMap;
+        } else {
+          Benefits = {}; // or handle the case when data is null
+        }
+      });
+    });
     return Scaffold(
       bottomSheet: Container(
         color: Colors.white,
-        padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-        child: isLastPage? getStarted() : Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: isLastPage
+            ? getStarted()
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //Skip Button
+                  TextButton(
+                      onPressed: () => pageController
+                          .jumpToPage(controller.items.length - 1),
+                      child: const Text('Skip')),
 
-            //Skip Button
-            TextButton(
-                onPressed: ()=>pageController.jumpToPage(controller.items.length-1),
-                child: const Text("Skip")),
+                  //Indicator
+                  SmoothPageIndicator(
+                    controller: pageController,
+                    count: controller.items.length,
+                    onDotClicked: (index) => pageController.animateToPage(index,
+                        duration: const Duration(milliseconds: 600),
+                        curve: Curves.easeIn),
+                    effect: const WormEffect(
+                      dotHeight: 12,
+                      dotWidth: 12,
+                      activeDotColor: primaryColor,
+                    ),
+                  ),
 
-            //Indicator
-            SmoothPageIndicator(
-                controller: pageController,
-                count: controller.items.length,
-                onDotClicked: (index)=> pageController.animateToPage(index,
-                    duration: const Duration(milliseconds: 600), curve: Curves.easeIn),
-                effect: const WormEffect(
-                  dotHeight: 12,
-                  dotWidth: 12,
-                  activeDotColor: primaryColor,
-                ),
-            ),
-
-            //Next Button
-            TextButton(
-                onPressed: ()=>pageController.nextPage(
-                    duration: const Duration(milliseconds: 600), curve: Curves.easeIn),
-                child: const Text("Next")),
-
-
-          ],
-        ),
+                  //Next Button
+                  TextButton(
+                      onPressed: () => pageController.nextPage(
+                          duration: const Duration(milliseconds: 600),
+                          curve: Curves.easeIn),
+                      child: const Text("Next")),
+                ],
+              ),
       ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 15),
         child: PageView.builder(
-            onPageChanged: (index)=> setState(()=> isLastPage = controller.items.length-1 == index),
+            onPageChanged: (index) => setState(
+                () => isLastPage = controller.items.length - 1 == index),
             itemCount: controller.items.length,
             controller: pageController,
-            itemBuilder: (context,index){
+            itemBuilder: (context, index) {
               return Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Image.asset(controller.items[index].image),
                   const SizedBox(height: 15),
-                  Text(controller.items[index].title,
-                    style: const TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+                  Text(
+                    controller.items[index].title,
+                    style: const TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
                   const SizedBox(height: 15),
                   Image.asset('lib/assets/logo.png'),
                 ],
               );
-
-        }),
+            }),
       ),
     );
   }
@@ -90,25 +118,27 @@ class _OnboardingViewState extends State<OnboardingView> {
 
   //Get started button
 
- Widget getStarted(){
+  Widget getStarted() {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: primaryColor
-      ),
+          borderRadius: BorderRadius.circular(8), color: primaryColor),
       width: MediaQuery.of(context).size.width * .9,
       height: 55,
       child: TextButton(
-          onPressed: ()async{
+          onPressed: () async {
             final pres = await SharedPreferences.getInstance();
             pres.setBool("onboarding", true);
 
             //After we press get started button this onboarding value become true
             // same key
-            if(!mounted)return;
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginOrSignupPage()));
+            if (!mounted) return;
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => LoginOrSignupPage()));
           },
-          child: const Text("Get started",style: TextStyle(color: Colors.white),)),
+          child: const Text(
+            "Get started",
+            style: TextStyle(color: Colors.white),
+          )),
     );
- }
+  }
 }
